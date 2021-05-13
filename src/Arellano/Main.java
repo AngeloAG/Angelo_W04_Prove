85b5f6554060346db1f4fe0cb20e7f678d1d91a8
Angelo <angelo.ag.079@gmail.com>
2021-05-13 16:42:25 -0500

    Initial commit

---
+++ src/Arellano/Main.java
@@ -0,0 +1,8 @@
+package Arellano;
+
+public class Main {
+    public static void main(String[] args) {
+        //Simple display message to screen
+        System.out.print("Hello, my name is Angelo.");
+    }
+}
